1530f48fed1f1e1d1b5ae1a870b18dedcbb5aa46
Fernan de Dios <info@mediagramstudios.com>
2018-11-22 04:27:27 +0800

    initial diagflow integration

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-require('./routes/dialogFlowRoutes');
+require('./routes/dialogFlowRoutes')(app);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,10 +1,33 @@
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
+
 module.exports = app => {
   app.get('/', (req, res) => {
     res.send({ 'hello': 'world' });
   });
 
-  app.post('/api/df_text_query', (req, res) => {
-    res.send({ 'do': 'text_query' })
+  app.post('/api/df_text_query', async (req, res) => {
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: req.body.text,
+          languageCode: config.dialogFlowSessionLanguageCode
+        }
+      }
+    }
+
+    try {
+      let responses = await sessionClient.detectIntent(request);
+      res.send(responses[0].queryResult);
+    }
+    catch(err) {
+      console.log('ERROR', err);
+    }
   });
 
   app.post('/api/df_event_query', (req, res) => {
