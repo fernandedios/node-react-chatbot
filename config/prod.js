0f9e65063f56c68f2c952b1d22d2a795d1bd0b19
Fernan de Dios <info@mediagramstudios.com>
2018-11-23 11:02:48 +0800

    added production / dev keys switching

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleProjectID: process.env.GOOGLE_PROJECT_ID,
+  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE
+};
