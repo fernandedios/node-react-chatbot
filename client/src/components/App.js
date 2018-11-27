38b4a5bf5a8eb4798f137ee96ff1e25cf28e9717
Fernan de Dios <info@mediagramstudios.com>
2018-11-28 05:33:34 +0800

    client routing setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import About from './pages/About';
+import Shop from './shop/Shop';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/shop" component={Shop} />
+      </BrowserRouter>
+    </div>
   );
 };
 
