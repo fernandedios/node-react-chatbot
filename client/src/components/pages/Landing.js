5111a44f4dd2b46ad66f7d45fc5b79dfe4115cb3
Fernan de Dios <info@mediagramstudios.com>
2018-11-28 03:43:26 +0800

    added boilerplate client pages

---
+++ client/src/components/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Selling great stuff!</h1>
+      with the help of the chatbot
+    </div>
+  );
+}
+
+export default Landing;
