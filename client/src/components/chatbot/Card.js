5fe47116749cbbb051f763e43e0bb88565cad2e6
Fernan de Dios <info@mediagramstudios.com>
2018-12-11 03:07:28 +0800

    added card component

---
+++ client/src/components/chatbot/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Card = (props) => {
+  const { header, image, description, link, price } = props.payload.fields;
+  return (
+    <div style={{ float: 'left', paddingRight: '30px', width: '270px' }}>
+      <div className="card">
+        <div className="card-image">
+          <img style={{ width: '240px'}} src={image.stringValue} />
+          <span className="card-title">{header.stringValue}</span>
+          <p>{price.stringValue}</p>
+        </div>
+        <div className="card-content">{description.stringValue}</div>
+        <div className="card-action">
+          <a target="_blank" href={link.stringValue}>GET NOW</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
